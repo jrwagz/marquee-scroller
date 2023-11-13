55969eb71e8e26f18c7588619bfee945d4fcc6fa
Justin Wagner <jrwagz@gmail.com>
2023-11-12 20:25:46 -0700

    Initial Working Prototype of Wagner Family Calendar Display (#1)
    These changes perform a few vital changes:
    
    - removes options that we won't be using to cut down on memory usage, so that we can enable an HTTPS client
    - Adds a new HTTPS client for pulling JSON data and displaying it (based on the previous News Client).
    
    ---------
    
    Co-authored-by: Dallan Wagner <dalwagne@adobe.com>

--- marquee/Settings.h
+++ marquee/Settings.h
@@ -48,9 +48,7 @@ SOFTWARE.
 #include <pgmspace.h>
 #include "OpenWeatherMapClient.h"
 #include "TimeDB.h"
-#include "NewsApiClient.h" 
-#include "OctoPrintClient.h"
-#include "PiHoleClient.h"
+#include "WagFamBdayClient.h"
 
 //******************************
 // Start Settings
@@ -59,7 +57,7 @@ SOFTWARE.
 String TIMEDBKEY = ""; // Your API Key from https://timezonedb.com/register
 String APIKEY = ""; // Your API Key from http://openweathermap.org/
 // Default City Location (use http://openweathermap.org/find to find city ID)
-int CityIDs[] = { 5304391 }; //Only USE ONE for weather marquee
+int CityIDs[] = { 5778244 }; //Only USE ONE for weather marquee
 String marqueeMessage = "";
 boolean IS_METRIC = false; // false = Imperial and true = Metric
 boolean IS_24HOUR = false; // 23:00 millitary 24 hour clock
@@ -67,16 +65,16 @@ boolean IS_PM = true; // Show PM indicator on Clock when in AM/PM mode
 const int WEBSERVER_PORT = 80; // The port you can access this device on over HTTP
 const boolean WEBSERVER_ENABLED = true;  // Device will provide a web interface via http://[ip]:[port]/
 boolean IS_BASIC_AUTH = false;  // Use Basic Authorization for Configuration security on Web Interface
-char* www_username = "admin";  // User account for the Web Interface
-char* www_password = "password";  // Password for the Web Interface
+char* www_username = (char*)"admin";  // User account for the Web Interface
+char* www_password = (char*)"password";  // Password for the Web Interface
 int minutesBetweenDataRefresh = 15;  // Time in minutes between data refresh (default 15 minutes)
 int minutesBetweenScrolling = 1; // Time in minutes between scrolling data (default 1 minutes and max is 10)
 int displayScrollSpeed = 25; // In milliseconds -- Configurable by the web UI (slow = 35, normal = 25, fast = 15, very fast = 5)
 boolean flashOnSeconds = true; // when true the : character in the time will flash on and off as a seconds indicator
 
-boolean NEWS_ENABLED = true;
-String NEWS_API_KEY = ""; // Get your News API Key from https://newsapi.org
-String NEWS_SOURCE = "reuters";  // https://newsapi.org/sources to get full list of news sources available
+boolean WAGFAM_CALENDAR_ENABLED = true;
+String WAGFAM_API_KEY = "";
+String WAGFAM_DATA_SOURCE_URL = "https://raw.githubusercontent.com/jrwagz/wagfam-birthday-clocks/main/data_source.json";
 
 // Display Settings
 // CLK -> D5 (SCK)  
@@ -96,21 +94,6 @@ int ledRotation = 3;
 
 String timeDisplayTurnsOn = "06:30";  // 24 Hour Format HH:MM -- Leave blank for always on. (ie 05:30)
 String timeDisplayTurnsOff = "23:00"; // 24 Hour Format HH:MM -- Leave blank for always on. Both must be set to work.
-
-// OctoPrint Monitoring -- Monitor your 3D printer OctoPrint Server
-boolean OCTOPRINT_ENABLED = false;
-boolean OCTOPRINT_PROGRESS = true;
-String OctoPrintApiKey = "";  // ApiKey from your User Account on OctoPrint
-String OctoPrintServer = "";  // IP or Address of your OctoPrint Server (DO NOT include http://)
-int OctoPrintPort = 80;       // the port you are running your OctoPrint server on (usually 80);
-String OctoAuthUser = "";     // only used if you have haproxy or basic athentintication turned on (not default)
-String OctoAuthPass = "";     // only used with haproxy or basic auth (only needed if you must authenticate)
-
-// Pi-hole Client -- monitor basic stats from your Pi-hole server (see http://pi-hole.net)
-boolean USE_PIHOLE = false;   // Set true to display your Pi-hole details
-String PiHoleServer = "";     // IP or Address only (DO NOT include http://)
-int PiHolePort = 80;          // Port of your Pi-hole address (default 80)
-String PiHoleApiKey = "";   // Optional -- only needed to see top blocked clients
 
 boolean ENABLE_OTA = true;    // this will allow you to load firmware to the device over WiFi (see OTA for ESP8266)
 String OTA_Password = "";     // Set an OTA password here -- leave blank if you don't want to be prompted for password
