24d9bf3d44e5fa08a0e88e9e6dc5ab7cc17a5cf4
Justin Wagner <jrwagz@gmail.com>
2026-05-03 14:55:40 -0600

    Bump version to 3.09.6-wagfam

--- marquee/marquee.ino
+++ marquee/marquee.ino
@@ -28,7 +28,7 @@
 #include "Settings.h"
 #include "SecurityHelpers.h"
 
-#define BASE_VERSION "3.09.5-wagfam"
+#define BASE_VERSION "3.09.6-wagfam"
 #ifdef BUILD_SUFFIX
 #define VERSION BASE_VERSION BUILD_SUFFIX
 #else
