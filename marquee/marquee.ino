a01d1731863c04a13cdc55d4838f5a3e58e142fa
Justin Wagner <jrwagz@gmail.com>
2026-05-12 10:21:15 -0600

    Bump patch version to 4.4.1-wagfam

--- marquee/marquee.ino
+++ marquee/marquee.ino
@@ -69,7 +69,7 @@ static const ScrollerFont SCROLLER_FONTS[] = {
 };
 static const int SCROLLER_FONT_COUNT = sizeof(SCROLLER_FONTS) / sizeof(SCROLLER_FONTS[0]);
 
-#define BASE_VERSION "4.4.0-wagfam"
+#define BASE_VERSION "4.4.1-wagfam"
 #ifdef BUILD_SUFFIX
 #define VERSION BASE_VERSION BUILD_SUFFIX
 #else
